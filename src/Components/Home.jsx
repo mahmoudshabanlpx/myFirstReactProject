e99921615caf29573adcf3351ae740f558a435fa
Mahmoud Shaban <mhmwdshbanmhmwd30@gmail.com>
2025-08-31 23:53:41 +0300

    second commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <div>
             <h1>Home Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, necessitatibus.</p>
-            <button onClick={()=>setCount(count++)}>Increment</button>
-            <button onClick={()=>setCount(count--)}>dencrement</button>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>dencrement</button>
             <p>Counter : {count}</p>
         </div>
     );
